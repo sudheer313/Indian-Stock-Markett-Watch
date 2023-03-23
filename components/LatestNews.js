b882e96b5937e5b1cebff0d2e10b0ed2101ef16f
sudheer313 <103562595+sudheer313@users.noreply.github.com>
2023-03-23 16:04:12 +1100

    update Home.js

--- components/LatestNews.js
+++ components/LatestNews.js
@@ -1,9 +1,48 @@
-import React from 'react'
+import React from "react";
+import NewsCard from "./NewsCard";
+
+const latestNews = [
+  {
+    id: 1,
+    title: "Indian Stock Market Hits All-Time High",
+    date: "March 24, 2023",
+    summary:
+      "The Indian stock market reached an all-time high today, driven by strong gains in the technology and finance sectors...",
+  },
+  {
+    id: 2,
+    title: "RBI Announces New Monetary Policy Measures",
+    date: "March 23, 2023",
+    summary:
+      "The Reserve Bank of India (RBI) announced a series of monetary policy measures aimed at boosting economic growth and controlling inflation...",
+  },
+  {
+    id: 3,
+    title: "Automobile Sector Shows Signs of Recovery",
+    date: "March 22, 2023",
+    summary:
+      "The automobile sector is showing signs of recovery after a prolonged slowdown, with sales numbers gradually picking up across the industry...",
+  },
+];
 
 const LatestNews = () => {
   return (
-    <div>LatestNews</div>
-  )
-}
+    <div>
+      <div>
+        <h2 className="text-2xl font-bold mb-6">Latest News</h2>
+        <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 gap-6">
+          {latestNews.map((news, index) => (
+            <NewsCard
+              key={index}
+              title={news.title}
+              date={news.date}
+              summary={news.summary}
+            />
+          ))}
+        </div>
+      </div>
+    </div>
+  );
+};
 
-export default LatestNews
+export default LatestNews;
--- pages/Home.js
+++ pages/Home.js
@@ -1,21 +1,22 @@
 import React from "react";
-import Header from "../components/Header";
 import Hero from "../components/Hero";
+import Section from "../components/Section";
+import LatestNews from "../components/LatestNews";
+import Blogs from "./Blogs";
+
 
 const Home = () => {
   return (
     <>
       <Hero />
-      <div className="container mx-auto px-4 py-8">
+      <Section>
         <h1 className="text-3xl font-bold mb-6">Indian Stock Market Watch</h1>
-
         <p className="mb-4">
           Welcome to the Indian Stock Market Watch, your one-stop source for the
           latest news, insights, and updates on the Indian stock market. Our
           goal is to provide you with accurate and timely information to help
           you make informed investment decisions.
         </p>
-
         <p className="mb-4">
           The Indian stock market has grown exponentially over the past few
           decades, attracting investors from all around the world. With a
@@ -23,7 +24,6 @@ const Home = () => {
           and infrastructure, the Indian market offers a wealth of opportunities
           for investors to explore.
         </p>
-
         <p className="mb-4">
           Our platform provides comprehensive coverage of the National Stock
           Exchange (NSE) and the Bombay Stock Exchange (BSE), along with
@@ -31,7 +31,6 @@ const Home = () => {
           addition, we provide in-depth analysis and expert opinions on
           individual stocks, sectors, and the overall market.
         </p>
-
         <p className="mb-4">
           Stay ahead in the world of investing by keeping yourself updated with
           our latest news, expert opinions, and insightful articles on the
@@ -39,7 +38,14 @@ const Home = () => {
           investor, our platform offers valuable resources to help you make
           well-informed decisions and achieve your financial goals.
         </p>
-      </div>
+      
+      </Section>
+      <Section backgroundColor="gray-100">
+        <LatestNews />
+      </Section>
+      <Section>
+        <Blogs />
+      </Section>
     </>
   );
 };
