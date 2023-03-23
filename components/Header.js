2f69e66a29789dc6d27a9ca98b8baf636bdb9697
sudheer313 <103562595+sudheer313@users.noreply.github.com>
2023-03-23 11:28:33 +1100

    update routing structure

--- components/Header.js
+++ components/Header.js
@@ -5,32 +5,32 @@ const Header = () => {
   return (
     <header className="bg-gray-800 py-4">
       <div className="container mx-auto flex justify-between items-center px-4">
-        <Link href="/">
-          <button className="text-white font-bold text-lg">My App</button>
+        <Link href="/Home">
+          <button className="text-white font-bold text-lg  hover:text-blue-500">Market-watch</button>
         </Link>
         <nav>
           <ul className="flex space-x-4 text-gray-300">
             <li>
-              <Link href="/">
+              <Link href="/Home">
                 <button className="text-white hover:text-blue-500">Home</button>
               </Link>
             </li>
             <li>
-              <Link href="/about">
+              <Link href="/About">
                 <button className="text-white hover:text-blue-500">
                   About
                 </button>
               </Link>
             </li>
             <li>
-              <Link href="/latest-news">
+              <Link href="/Latestnews">
                 <button className="text-white hover:text-blue-500">
                   Latest News
                 </button>
               </Link>
             </li>
             <li>
-              <Link href="/blogs">
+              <Link href="/Blogs">
                 <button className="text-white hover:text-blue-500">
                   Blogs
                 </button>
---
+++ pages/Latestnews.js
@@ -0,0 +1,13 @@
+import React from "react";
+import Header from "../components/Header";
+
+const Latestnews = () => {
+  return (
+    <div>
+      <Header />
+      Latestnews
+    </div>
+  );
+};
+
+export default Latestnews;
