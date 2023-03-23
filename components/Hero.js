9bf5c4f83bf0e6347f884bb3eae52bd44fad1dcb
sudheer313 <103562595+sudheer313@users.noreply.github.com>
2023-03-23 16:23:17 +1100

    updated hero with image

--- components/Hero.js
+++ components/Hero.js
@@ -5,15 +5,15 @@ const Hero = () => {
     <div>
       <div
         className="bg-cover bg-center h-96 text-white py-24 px-10 object-fill"
-        style={{ backgroundImage: 'url("https://source.unsplash.com/random")' }}
+        style={{ backgroundImage: 'url("/img1.jpg")' }}
       >
-        <h1 className="text-4xl font-bold mb-4">
+        <h1 className=" hover:text-blue-700 text-4xl font-bold mb-4">
           Welcome to the Indian Stock Market Watch
         </h1>
-        <p className="text-2xl mb-12">
+        <p className=" hover:text-blue-700 text-2xl mb-12">
           Stay up-to-date with the latest news, blogs, and market insights
         </p>
-        <button className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">
+        <button className="text-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">
           Get Started
         </button>
       </div>
