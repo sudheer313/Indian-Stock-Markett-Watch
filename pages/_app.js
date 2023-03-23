382ec04a12721c608bb727b82ca8dbc43ab077e3
sudheer313 <103562595+sudheer313@users.noreply.github.com>
2023-03-23 19:10:09 +1100

    Added Google Analytics tracking to the APP

--- pages/_app.js
+++ pages/_app.js
@@ -1,9 +1,18 @@
 import "tailwindcss/tailwind.css";
 import "../styles/globals.css";
-import Header from '../components/Header'; // Import the Header component
-import Footer from '../components/Footer'; // Import the Footer component
+import Header from "../components/Header"; // Import the Header component
+import Footer from "../components/Footer"; // Import the Footer component
+import ReactGA from "react-ga";
+import { useEffect } from "react";
+import { useRouter } from "next/router";
 
 function MyApp({ Component, pageProps }) {
+  const router = useRouter();
+
+  useEffect(() => {
+    ReactGA.initialize("G-9CW1J527WZ");
+    ReactGA.pageview(window.location.pathname + window.location.search);
+  }, []);
   return (
     <div className="flex flex-col min-h-screen">
       <Header />
@@ -16,4 +25,3 @@ function MyApp({ Component, pageProps }) {
 }
 
 export default MyApp;
-
