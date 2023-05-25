8139046b93e985b7a94bcc20c9f3a87e99c5fef4
sudheer313 <103562595+sudheer313@users.noreply.github.com>
2023-05-25 19:04:18 +1000

    update hero componnet with text color update

--- components/Hero.js
+++ components/Hero.js
@@ -1,15 +1,15 @@
-import styles from '../styles/Hero.module.css';
+import styles from "../styles/Hero.module.css";
 
 const Hero = () => {
   return (
     <div className={`${styles.heroImage} h-96 py-24 px-10`}>
-      <h1 className="text-4xl font-bold text-red-400 mb-4 shadow-lg">
-        Welcome to the  Indian Stock Market Watch
+      <h1 className="text-4xl font-bold text-red-900 mb-4 shadow-lg">
+        Welcome to the Indian Stock Market Watch
       </h1>
-      <p className="text-2xl text-red-400 mb-12 shadow-lg">
+      <p className="text-2xl text-red-900 mb-12 shadow-lg">
         Stay up-to-date with the latest news, blogs, and market insights
       </p>
-      <button className="bg-yellow-500 hover:bg-yellow-700 text-black font-bold py-2 px-4 rounded">
+      <button className="bg-yellow-500 hover:bg-yellow-700 text-black font-bold py-2 px-4 rounded shadow-lg">
         Get Started
       </button>
     </div>
