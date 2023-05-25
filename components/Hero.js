18e73d2a511a1ad823daf6dc8dfc94f10ef76780
sudheer313 <103562595+sudheer313@users.noreply.github.com>
2023-05-25 19:32:58 +1000

    update Hero.js tect content

--- components/Hero.js
+++ components/Hero.js
@@ -3,10 +3,10 @@ import styles from "../styles/Hero.module.css";
 const Hero = () => {
   return (
     <div className={`${styles.heroImage} h-96 py-24 px-10`}>
-      <h1 className="text-4xl font-bold text-red-900 mb-4 shadow-lg">
+      <h1 className="text-4xl font-bold text-white-900 mb-4 shadow-lg">
         Welcome to the Indian Stock Market Watch
       </h1>
-      <p className="text-2xl text-red-900 mb-12 shadow-lg">
+      <p className="text-2xl font-bold text-white-900 mb-12 shadow-lg">
         Stay up-to-date with the latest news, blogs, and market insights
       </p>
       <button className="bg-yellow-500 hover:bg-yellow-700 text-black font-bold py-2 px-4 rounded shadow-lg">
