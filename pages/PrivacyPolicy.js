eb87724db5bf1323451503ac050ada76edc87b51
sudheer313 <103562595+sudheer313@users.noreply.github.com>
2023-05-26 11:41:28 +1000

    update privacy statement in pages/Privacy policy

--- pages/PrivacyPolicy.js
+++ pages/PrivacyPolicy.js
@@ -8,49 +8,56 @@ const PrivacyPolicy = () => {
       <p className="mb-4">
         Your privacy is important to us. It is our policy to respect your
         privacy regarding any information we may collect from you across our
-        website, [your_website], and other sites we own and operate.
+        website and other sites we own and operate.
       </p>
 
       <p className="mb-4">
         We only ask for personal information when we truly need it to provide a
         service to you. We collect it by fair and lawful means, with your
-        knowledge and consent. We also let you know why we’re collecting it and
+        knowledge and consent. We also let you know why we're collecting it and
         how it will be used.
       </p>
 
       <p className="mb-4">
-        We only retain collected information for as long as necessary to provide
-        you with your requested service. What data we store, we’ll protect
-        within commercially acceptable means to prevent loss and theft, as well
-        as unauthorized access, disclosure, copying, use, or modification.
+        We may collect certain types of information when you visit our website
+        or interact with our services, such as your IP address, browser type,
+        operating system, and other usage details. This information is used to
+        analyze trends, administer the site, track user movements, and gather
+        broad demographic information for aggregate use. It helps us understand
+        how visitors engage with our website and improve our services.
+      </p>
+      <p className="mb-4">
+        Please note that we are not a SEBI registered advisory. The information
+        provided on our website is for general informational purposes only and
+        should not be considered as professional advice. We do not provide
+        investment recommendations or financial guidance. Any investment or
+        trading decisions you make are solely your responsibility.
       </p>
 
       <p className="mb-4">
-        We don’t share any personally identifying information publicly or with
-        third-parties, except when required to by law.
+        We store your personal information securely and take all reasonable
+        measures to protect it from unauthorized access, disclosure, or
+        alteration. We do not share your personal information with third
+        parties, except as required by law or with your explicit consent.
       </p>
 
       <p className="mb-4">
-        Our website may link to external sites that are not operated by us.
-        Please be aware that we have no control over the content and practices
-        of these sites, and cannot accept responsibility or liability for their
-        respective privacy policies.
+        Our website may contain links to external sites that are not operated by
+        us. Please be aware that we have no control over the content and
+        practices of these sites and cannot accept responsibility or liability
+        for their respective privacy policies.
       </p>
 
       <p className="mb-4">
-        You are free to refuse our request for your personal information, with
-        the understanding that we may be unable to provide you with some of your
-        desired services.
+        By using our website, you consent to the collection and use of your
+        information as described in this Privacy Policy. If you have any
+        questions or concerns about our privacy practices, please contact us.
       </p>
 
       <p className="mb-4">
-        Your continued use of our website will be regarded as acceptance of our
-        practices around privacy and personal information. If you have any
-        questions about how we handle user data and personal information, feel
-        free to contact us.
+        This Privacy Policy is effective as of today and may be updated from
+        time to time. Any changes will be posted on this page.
       </p>
-
-      <p className="mb-4">This policy is effective as of 23/03/23.</p>
     </div>
   );
 };
