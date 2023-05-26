a4caeb2fc443198ca5aa268f675af8aa97356ea0
sudheer313 <103562595+sudheer313@users.noreply.github.com>
2023-05-26 11:25:48 +1000

    update and include  page/Robotics.js

---
+++ pages/Robotics.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+const Robotics = () => {
+  return (
+    <div>
+      <h1>Robotics Page</h1>
+      <p>This is the content of the Robotics page.</p>
+    </div>
+  );
+};
+
+export default Robotics;
