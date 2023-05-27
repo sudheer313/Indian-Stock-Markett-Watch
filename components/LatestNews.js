aeb4eee1022d1f8e28c1eca941edf0750c70bb84
sudheer313 <103562595+sudheer313@users.noreply.github.com>
2023-05-28 09:57:28 +1000

    update latest news in component and pages

--- components/LatestNews.js
+++ components/LatestNews.js
@@ -4,24 +4,32 @@ import NewsCard from "./NewsCard";
 const latestNews = [
   {
     id: 1,
-    title: "Indian Stock Market Hits All-Time High",
-    date: "March 24, 2023",
+    title: "Yellen Faces Fallout After Debt Limit Warnings Ignored",
+    date: "May 27, 2023",
     summary:
-      "The Indian stock market reached an all-time high today, driven by strong gains in the technology and finance sectors...",
+      "U.S. Secretary of the Treasury, Yellen's cautions regarding the debt limit were not taken into consideration, now leaving her to tackle the resulting consequences. She has kept her contingency plans private but indicated she has preparations in place to deal with potential adverse outcomes.",
   },
   {
     id: 2,
-    title: "RBI Announces New Monetary Policy Measures",
-    date: "March 23, 2023",
+    title: "Aurobindo Pharma's Q4 Net Profit Drops by 12%, Revenue Rises 11%",
+    date: "May 27, 2023",
     summary:
-      "The Reserve Bank of India (RBI) announced a series of monetary policy measures aimed at boosting economic growth and controlling inflation...",
+      "Aurobindo Pharma's net profit in Q4 has seen a 12% slump, standing at Rs 506 crore, whereas revenue saw a 11% increase. Q4FY23 EBITDA rose 3% from Rs 974.4 crore in the previous year, amounting to Rs 1,002 crore.",
   },
   {
     id: 3,
-    title: "Automobile Sector Shows Signs of Recovery",
-    date: "March 22, 2023",
+    title:
+      "Union Ministers Laud PM Modi's 'Decisive Leadership' Marking 9 Years in Government",
+    date: "May 27, 2023",
     summary:
-      "The automobile sector is showing signs of recovery after a prolonged slowdown, with sales numbers gradually picking up across the industry...",
+      "At the national conclave marking 9 years of Modi government, union ministers commended PM's resolute leadership. Finance Minister Nirmala Sitharaman highlighted PM Modi's transformative mindset and his 'Seva Bhaav', earning him the trust of the Indian populace.",
+  },
+  {
+    id: 4,
+    title: "Power Finance Corp's Net Profit Increases by 44% in March Quarter",
+    date: "May 27, 2023",
+    summary:
+      "Power Finance Corp reported a 44% rise in its net profit for the March quarter, amounting to Rs 6,128 crore. The company's consolidated net profit was Rs 4,295.90 crore in the same quarter of the previous year, as per a BSE filing.",
   },
 ];
 
--- pages/Latestnews.js
+++ pages/Latestnews.js
@@ -1,42 +1,13 @@
 import React from "react";
 import Header from "../components/Header";
 
-const newsData = [
-  {
-    id: 1,
-    title: "Cryptocurrency Market Faces Major Correction",
-    date: "May 25, 2023",
-    summary:
-      "The global cryptocurrency market took a steep dive today, facing a major correction. The top cryptocurrencies, including Bitcoin and Ethereum, faced significant losses. Market analysts are closely monitoring the situation.",
-  },
-  {
-    id: 2,
-    title: "EU to Implement New Digital Tax on Tech Giants",
-    date: "May 24, 2023",
-    summary:
-      "The European Union has announced plans to implement a new digital tax on tech giants. The move is seen as an effort to increase tax revenues from multinational companies operating within the digital space.",
-  },
-  {
-    id: 3,
-    title: "Oil Prices Soar Amid Increased Global Demand",
-    date: "May 23, 2023",
-    summary:
-      "Oil prices are on the rise again amid increasing global demand and supply constraints. The Brent Crude price has crossed the $80 per barrel mark for the first time in three years.",
-  },
-  {
-    id: 4,
-    title: "Tesla Announces New Affordable Model",
-    date: "May 22, 2023",
-    summary:
-      "Tesla has announced a new, more affordable model in their line-up. The new vehicle is expected to make electric cars more accessible to a larger consumer base.",
-  },
-];
+
 
 const Latestnews = () => {
   return (
     <div className="container mx-auto px-4 py-8">
       <h1 className="text-3xl font-bold mb-6">Latest News</h1>
-      
+
       {newsData.map((newsItem) => (
         <div
           key={newsItem.id}
