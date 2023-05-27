4b570e0238d30cfe8c0f4d0c3126ef999b0a7499
sudheer313 <103562595+sudheer313@users.noreply.github.com>
2023-05-27 21:21:11 +1000

    update BlogPost.js in components

---
+++ components/BlogPost.js
@@ -0,0 +1,56 @@
+import React, { useEffect, useState } from "react";
+import Head from "next/head";
+import Header from "../../components/Header";
+
+const BlogPost = ({ slug }) => {
+  const [blogPostData, setBlogPostData] = useState(null);
+
+  useEffect(() => {
+    const fetchBlogPostData = async () => {
+      try {
+        const response = await fetch(
+          `https://api.bharatbullbear.com/api/blog/${slug}`
+        );
+        const data = await response.json();
+
+        if (response.ok) {
+          setBlogPostData(data); // Set the blog post data if the response is successful
+        } else {
+          throw new Error(data.message); // Handle error if the response is not successful
+        }
+      } catch (error) {
+        console.error(error);
+        // Handle error in fetching the blog post data
+      }
+    };
+
+    fetchBlogPostData();
+  }, [slug]);
+
+  if (!blogPostData) {
+    // Handle loading state or error state
+    return <div>Loading...</div>;
+  }
+
+  const { title, date, summary, content } = blogPostData;
+
+  return (
+    <>
+      <Head>
+        <title>{title} | Bharat Bull Bear</title>
+        {/* Add meta tags for SEO */}
+      </Head>
+
+      <Header />
+
+      <div className="container mx-auto px-4 py-8">
+        <h1>{title}</h1>
+        <p>{date}</p>
+        <p>{summary}</p>
+        <div dangerouslySetInnerHTML={{ __html: content }}></div>
+      </div>
+    </>
+  );
+};
+
+export default BlogPost;
