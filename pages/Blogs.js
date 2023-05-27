4752232dcc8c7c1e2e3580c97371db80df4951d5
sudheer313 <103562595+sudheer313@users.noreply.github.com>
2023-05-27 20:45:08 +1000

    update pages/blog.js

--- pages/Blogs.js
+++ pages/Blogs.js
@@ -1,5 +1,4 @@
 import Head from "next/head";
-import Link from "next/link";
 import React from "react";
 import Header from "../components/Header";
 
@@ -10,19 +9,23 @@ const blogPosts = [
     summary:
       "Learn the basics of investing in the Indian stock market and discover tips and strategies for successful investments.",
     slug: "investing-in-the-indian-stock-market-a-beginners-guide",
+    link: "/blog/investing-in-the-indian-stock-market-a-beginners-guide",
   },
-
   {
     title: "The Future of Technology Stocks in India",
     date: "March 18, 2023",
     summary:
       "Explore the growth potential of technology stocks in India and understand the factors driving their performance.",
+    slug: "the-future-of-technology-stocks-in-india",
+    link: "/blog/the-future-of-technology-stocks-in-india",
   },
   {
     title: "How to Diversify Your Investment Portfolio",
     date: "March 15, 2023",
     summary:
       "Discover the benefits of diversification and learn how to create a diversified investment portfolio to minimize risk and maximize returns.",
+    slug: "how-to-diversify-your-investment-portfolio",
+    link: "/blog/how-to-diversify-your-investment-portfolio",
   },
 ];
 
@@ -40,14 +43,15 @@ const Blogs = () => {
         <h2 className="text-2xl font-bold mb-6">Latest Blog Posts</h2>
 
         <div className="space-y-6">
-          {blogPosts.map((blogPost, index) => (
-            <div key={index} className="border-b border-gray-300 pb-4">
+          {blogPosts.map((blogPost) => (
+            <div key={blogPost.slug} className="border-b border-gray-300 pb-4">
               <h3 className="text-xl font-semibold mb-2">
-                <Link href={`/blog/${blogPost.slug}`}>
-                  <button className="text-blue-500 hover:text-blue-700">
-                    {blogPost.title}
-                  </button>
-                </Link>
+                <a
+                  href={blogPost.link}
+                  className="text-blue-500 hover:text-blue-700"
+                >
+                  {blogPost.title}
+                </a>
               </h3>
               <p className="text-sm text-gray-500 mb-2">{blogPost.date}</p>
               <p className="text-gray-700">{blogPost.summary}</p>
