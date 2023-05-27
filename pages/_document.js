51ccdefbf4ff9ea08d2643d32d18146f2b799c82
sudheer313 <103562595+sudheer313@users.noreply.github.com>
2023-05-27 19:44:45 +1000

    update pages/_document.js with google adsense code

--- pages/_document.js
+++ pages/_document.js
@@ -1,13 +1,19 @@
-import { Html, Head, Main, NextScript } from 'next/document'
+import { Html, Head, Main, NextScript } from "next/document";
 
 export default function Document() {
   return (
     <Html lang="en">
-      <Head />
+      <Head>
+        <script
+          async
+          src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-4062165141789437"
+          crossorigin="anonymous"
+        ></script>
+      </Head>
       <body>
         <Main />
         <NextScript />
       </body>
     </Html>
-  )
+  );
 }
