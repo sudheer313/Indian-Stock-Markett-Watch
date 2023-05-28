1c1379ae2901d034c2c546bd9db5a6584e7c47d0
sudheer313 <103562595+sudheer313@users.noreply.github.com>
2023-05-28 18:33:50 +1000

    update google site tag in _document.js

--- pages/_document.js
+++ pages/_document.js
@@ -5,11 +5,29 @@ export default function Document() {
     <Html lang="en">
       <Head>
         <meta charSet="UTF-8" />
+
+        {/* Google AdSense */}
         <script
           async
           src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-4062165141789437"
           crossOrigin="anonymous"
         ></script>
+
+        {/* Global Site Tag (gtag.js) - Google Analytics */}
+        <script
+          async
+          src={`https://www.googletagmanager.com/gtag/js?id=G-C2BQK1V38E`}
+        />
+        <script
+          dangerouslySetInnerHTML={{
+            __html: `
+              window.dataLayer = window.dataLayer || [];
+              function gtag(){dataLayer.push(arguments);}
+              gtag('js', new Date());
+              gtag('config', 'G-C2BQK1V38E');
+            `,
+          }}
+        />
       </Head>
       <body>
         <Main />
