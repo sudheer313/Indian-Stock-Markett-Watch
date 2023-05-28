419767dcf0d6e05d41d642b5b8186388a1c14553
sudheer313 <103562595+sudheer313@users.noreply.github.com>
2023-05-28 19:38:38 +1000

    update Latestnews with adComponnet

--- pages/Latestnews.js
+++ pages/Latestnews.js
@@ -1,11 +1,17 @@
 import React from 'react';
 import LatestNews from '../components/LatestNews';
+import AdComponent from '../components/AdComponent';
 
 const Latestnews = () => {
     return (
         <div>
             <h1>Latest News</h1>
             <LatestNews />
+
+            {/* Add the AdComponent */}
+            <div className="mt-8">
+                <AdComponent />
+            </div>
         </div>
     );
 };
